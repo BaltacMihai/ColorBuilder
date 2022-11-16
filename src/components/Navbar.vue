720596c8ada73822864f63c38935d9783019a7b9
Mihai-Cristian Bâltac <baltacmihaicristian@gmail.com>
2022-11-16 18:31:13 +0200

    vue 3 created + navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar">
+    <RouterLink to="/" class="logo">
+      <img src="./../assets/imgaes/logo.svg" alt="Logo "
+    /></RouterLink>
+
+    <h1>{{ title }}</h1>
+  </nav>
+</template>
+
+<script>
+import { RouterLink } from "vue-router";
+
+export default {
+  data() {
+    return {
+      title: "",
+    };
+  },
+
+  watch: {
+    $route(to, from) {
+      this.title = `${this.$route.name
+        .charAt(0)
+        .toUpperCase()}${this.$route.name.slice(1)}`;
+    },
+  },
+};
+</script>
